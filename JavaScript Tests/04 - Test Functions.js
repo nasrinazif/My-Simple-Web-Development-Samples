4b5e04d7c6f38f40387a3f997ff676350f3a98d2
nasrinazif <nasri@dal.ca>
2019-11-24 11:47:06 +0330

    isEven, factorial and kebabToSnake functions were added to 04 - Test Functions.js file!

---
+++ JavaScript Tests/04 - Test Functions.js
@@ -0,0 +1,22 @@
+function isEven(num){
+  if(num % 2 == 0){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+function factorial(numFact){
+  result = 1;
+  while(numFact > 0){
+    result *= numFact;
+    numFact -= 1;
+  }
+  return result;
+}
+
+function kebabToSnake(str){
+  var snakeResult = str.replace(/-/g, "_");
+  return snakeResult;
+}
