36296a655d4ea8ced0ffdf3688d785e9147148bb
nasrinazif <nasri@dal.ca>
2019-11-26 10:49:39 +0330

    Changed to functions!

--- JavaScript Tests/05 - Test - ToDo List Project.js
+++ JavaScript Tests/05 - Test - ToDo List Project.js
@@ -2,42 +2,36 @@ window.setTimeout(function() {
   var userResponse;
   var todoList = [];
 
-  function todoAddNewItem(){
+  function todoAddNewItem() {
     item = prompt("*** Enter an item to be added to your todo list! ***");
-    return todoList.push(item);
-    console.log(" Item " + "\" " + item + "\"" + " was added to the todo list!")
+    todoList.push(item);
+    console.log("Item " + "\" " + item + "\"" + " was added to the todo list!")
   }
 
-  function todoList(){
+  function todoListItems() {
     console.log("******* The Todo List Items: *******");
-    for(i=0; i<todoList.length; i++){
+    for (i = 0; i < todoList.length; i++) {
       console.log(i + ": " + todoList[i])
     }
     console.log("************************************");
   }
 
-  function todoDeleteAnIndex(){
-    console.log("******* The Todo List Items: *******");
-    for(i=0; i<todoList.length; i++){
-      console.log(i + ": " + todoList[i])
-    }
-    console.log("************************************");
+  function todoDeleteAnIndex() {
+    deleteIndex = prompt("*** Enter the index of the item you want to delete! ***");
+    todoList.splice(deleteIndex, 1);
+    console.log("Item number " + "\" " + deleteIndex + "\"" + " was removed from the todo list!")
   }
 
-  while(userResponse != "quit"){
+  while (userResponse != "quit") {
 
     userResponse = prompt("What do you want to do?");
     var item;
 
-    if(userResponse === "new"){
+    if (userResponse === "new") {
       todoAddNewItem();
-    }
-
-    else if(userResponse === "list"){
-      todoList();
-    }
-
-    else if(userResponse === "delete"){
+    } else if (userResponse === "list") {
+      todoListItems();
+    } else if (userResponse === "delete") {
       todoDeleteAnIndex();
     }
 
