4618a4ff5dba8f37cbd6a6feb0b35c1c5f21184a
nasrinazif <nasri@dal.ca>
2019-11-26 11:22:02 +0330

    Array challenge 1 completed!

--- JavaScript Tests/06 - Test - Array Challenges.js
+++ JavaScript Tests/06 - Test - Array Challenges.js
@@ -1,1 +1,30 @@
-alert("111");
+
+var myList = [];
+var userInput = "sds";
+
+while(userInput != "quit"){
+  userInput = prompt("Add an item to the list!");
+  if(userInput != "quit"){
+    myList.push(userInput);
+  }
+}
+
+console.log("****** The List Items Are: ******");
+myList.forEach(function(item, i){
+  console.log(i + ": " + item);
+});
+console.log("*********************************");
+
+printReverse(myList);
+
+function printReverse(myList){
+  console.log("");
+  console.log("---------------------------------");
+  console.log("The Reverse of Your Items is: ");
+  tempList = myList;
+  while(tempList.length != 0){
+    popedItem = tempList.pop();
+    console.log(popedItem);
+  }
+  console.log("---------------------------------");
+}
