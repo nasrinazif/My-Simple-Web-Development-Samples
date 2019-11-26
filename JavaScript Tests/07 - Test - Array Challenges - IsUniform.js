c1e9ec96027841f9ec6fdb36c3c7a7db35971a39
nasrinazif <nasri@dal.ca>
2019-11-26 14:49:38 +0330

    Changes to challenge IsUniform made!

---
+++ JavaScript Tests/07 - Test - Array Challenges - IsUniform.js
@@ -0,0 +1,37 @@
+var myList = [];
+var userInput = "sds";
+
+while(userInput != "quit"){
+  userInput = prompt("Add an item to the list!");
+  if(userInput != "quit"){
+    myList.push(userInput);
+  }
+}
+
+console.log("****** The List Items Are: ******");
+myList.forEach(function(item, i){
+  console.log(i + ": " + item);
+});
+console.log("*********************************");
+
+result = true;
+for(i=0; i<myList.length; i++){
+  for(j=i+1; j<myList.length; j++){
+    if(myList[i] != myList[j]){
+      result = false;
+    }
+  }
+}
+
+if(result == true){
+  console.log("")
+  console.log("----------------------------------");
+  console.log("Yeah, All items are identical!");
+  console.log("----------------------------------");
+}
+else{
+  console.log("")
+  console.log("----------------------------------");
+  console.log("Sorry, All items are NOT identical!");
+  console.log("----------------------------------");
+}
