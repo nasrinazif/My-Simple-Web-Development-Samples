add911929c21ba1737447093fccd5e09e2126f66
nasrinazif <nasri@dal.ca>
2019-12-16 11:37:24 +0330

    Changes Made to Test Events JS

--- DOM Tests/01 - Test Events.js
+++ DOM Tests/01 - Test Events.js
@@ -1,1 +1,11 @@
-var todoList = document.getElementsByTagName(h1)
+var todoItems = document.querySelectorAll(".todoitem");
+
+console.log(todoItems);
+console.log(todoItems.length);
+
+for (var i = 0; i < todoItems.length; i++) {
+    todoItems[i].addEventListener("click", function() {
+        this.classList.toggle("todoChecked");
+    })
+
+}
