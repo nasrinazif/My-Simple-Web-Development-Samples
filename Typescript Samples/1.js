d4c2d3cd19c8caf7d774484dc4e50dbd8da750c3
nasrinazif <nasri@dal.ca>
2019-12-25 09:33:12 +0330

    Typescript example folder plus the first tests add

---
+++ Typescript Samples/1.js
@@ -0,0 +1,5 @@
+var message = "Ali Nasri's first tsc";
+var fName = "Ali";
+var personalID = 13515441;
+console.log(message);
+console.log(fName);
---
+++ Typescript Samples/1.ts
@@ -0,0 +1,5 @@
+var message:string = "Ali Nasri's first tsc" 
+let fName:string = "Ali"
+let personalID:number = 13515441
+console.log(message)
+console.log(fName)
