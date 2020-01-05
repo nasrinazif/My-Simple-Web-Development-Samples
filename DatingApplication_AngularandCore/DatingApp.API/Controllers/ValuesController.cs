b75c541ffc42651fc6ce266062cb4315974533fd
nasrinazif <nasri@dal.ca>
2020-01-05 11:37:13 +0330

    Values now have been read from the sqlite database created inn previous session.

--- DatingApplication_AngularandCore/DatingApp.API/Controllers/ValuesController.cs
+++ DatingApplication_AngularandCore/DatingApp.API/Controllers/ValuesController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DatingApp.API.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DatingApp.API.Controllers
@@ -10,18 +11,29 @@ namespace DatingApp.API.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+
+        }
+
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public IActionResult GetValues()
         {
-            return new string[] { "Ali", "Nasri" };
+            var values = _context.Values.ToList();
+
+            return Ok(values);
         }
 
         // GET api/values/5
-        [HttpGet("{myid}")]
-        public ActionResult<string> Get(int myid)
+        [HttpGet("{id}")]
+        public IActionResult GetValue(int id)
         {
-            return "your value is: " + myid;
+            var value = _context.Values.FirstOrDefault(x => x.Id == id);
+
+            return Ok(value);
         }
 
         // POST api/values
