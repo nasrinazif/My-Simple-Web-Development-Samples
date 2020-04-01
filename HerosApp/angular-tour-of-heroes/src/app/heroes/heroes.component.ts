29a97231a37487155eee8f469dc9596191a0c69b
nasrinazif <nasri@dal.ca>
2020-04-01 14:39:38 +0430

    Hero Service Changed to Async

--- HerosApp/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ HerosApp/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 
 @Component({
@@ -11,17 +11,23 @@ import { HEROES } from '../mock-heroes';
 
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes: Hero[];
 
   selectedHero: Hero;
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+  constructor(private heroService: HeroService) {
   }
 
-  constructor() { }
-
   ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+   this.heroService.getHeroes().subscribe(returnedHeroes => this.heroes = returnedHeroes);
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
