58ed00277ce4003af0de8031e7ad692dc996b9af
nasrinazif <nasri@dal.ca>
2020-04-02 16:27:18 +0430

    app-routing module added for routing

--- HerosApp/angular-tour-of-heroes/src/app/app.module.ts
+++ HerosApp/angular-tour-of-heroes/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
 
 
 
@@ -18,7 +19,8 @@ import { MessagesComponent } from './messages/messages.component';
    ],
    imports: [
       BrowserModule,
-      FormsModule
+      FormsModule,
+      AppRoutingModule
    ],
    providers: [],
    bootstrap: [
